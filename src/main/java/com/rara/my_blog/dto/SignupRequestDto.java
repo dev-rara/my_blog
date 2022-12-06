a01b42eb6251ad8b5150d155ac9582f66cc68d02
rara-dev <kksr1219@gmail.com>
2022-12-06 12:23:34 +0900

    비밀번호 유효성 검사 조건 추가

--- src/main/java/com/rara/my_blog/dto/SignupRequestDto.java
+++ src/main/java/com/rara/my_blog/dto/SignupRequestDto.java
@@ -14,6 +14,6 @@ public class SignupRequestDto {
 	private String username;
 
 	@NotBlank(message = "비밀번호는 필수 입력값 입니다.")
-	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,15}$", message = "비밀번호는 알파벳 대소문자, 숫자로 구성된 8~15자리여야 합니다.")
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&].{8,15}$", message = "비밀번호는 알파벳 대소문자, 숫자, 특수문자로 구성된 8~15자리여야 합니다.")
 	private String password;
 }
