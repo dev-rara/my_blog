09ecda6f73b058a895380429ce79781900b93341
rara-dev <kksr1219@gmail.com>
2022-12-14 10:40:21 +0900

    api 설계 수정

--- src/main/java/com/rara/my_blog/controller/CommentController.java
+++ src/main/java/com/rara/my_blog/controller/CommentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/comment")
+@RequestMapping("/api/comments")
 public class CommentController {
 	private final CommentService commentService;
 
